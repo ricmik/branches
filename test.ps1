c2701381d77d0db00a32cd6c5f4454e024e649c7
ricmik <richardmikalsen@gmail.com>
2023-08-21 14:04:51 +0200

    changed token

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 $password = "AnnaiI##sjsjs"
 $username = "asjai@goaoa.so"
-$token = "jaiosjaoidjsioajdoisajdoiajOIASJDIASDJIASJDIAJSDIJASIDJIAJSIJAOOOOOO"
+$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJhYmNkMTIzIiwiZXhwaXJ5IjoxNjQ2NjM1NjExMzAxfQ.3Thp81rDFrKXr3WrY1MyMnNK8kKoZBX9lg-JwFznR-M"
 
 
 $headers = @{
