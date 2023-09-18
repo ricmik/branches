7e78c75866e728f5e6ae16b52923b271110cbf44
ricmik <richardmikalsen@gmail.com>
2023-09-18 14:51:32 +0200

    Added that

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 $password = "AnnaiI##sjsjs"
 $username = "asjai@goaoa.so"
 $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJhYmNkMTIzIiwiZXhwaXJ5IjoxNjQ2NjM1NjExMzAxfQ.3Thp81rDFrKXr3WrY1MyMnNK8kKoZBX9lg-JwFznR-M"
-
+$test = "yes"
 
 $headers = @{
     password = $password
